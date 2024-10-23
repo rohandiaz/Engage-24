602e81e03da2e5439d9f658e0f8358eab10a8caa
Sayan Das <sayandasraja56@gmail.com>
2024-10-23 16:09:43 +0530

    cors ste up

--- Smart_Bite/server/server.js
+++ Smart_Bite/server/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: '*',
+    origin: 'https://smart-bite.onrender.com',
     credentials: true,
     optionSuccessStatus: 200
 }));
